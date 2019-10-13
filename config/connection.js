32ce255e167e8379dc12df2971f3565d101224a9
Abdu <etabdi@yahoo.com>
2019-10-13 18:10:49 -0500

    jawsdb setup

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,14 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        root: 8080,
-        host: process.env.DB_HOST,
+      
+        host: 'localhost',
         user: 'root',
         password: '',
         database: 'burger_db'
     });
 }
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+
 
 module.exports = connection;
