9abcdfc011f777250eb551f994d7436556303750
Abdu <etabdi@yahoo.com>
2019-10-13 23:20:50 -0500

    refresh database

--- server.js
+++ server.js
@@ -11,13 +11,21 @@ const PORT = process.env.PORT || 8080;
 
 // Config Settings
 app.use(methodOverride('_method'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
 app.set("view engine","handlebars");
+
 app.use("/", routes);
+
 app.use(express.static('public'));
+
 // Listener
 app.listen(PORT,function(){
+    
     console.log("App now listening at localhost:" + PORT);
 });
